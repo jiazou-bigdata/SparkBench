91ff38338f2a4da0d9bfff1614dad370885d9c1a
jiazou-bigdata <jacquelinezou@gmail.com>
2018-02-02 21:10:37 +0000

    extract spark event logs

--- scripts/spark-event-log-extract.py
+++ scripts/spark-event-log-extract.py
@@ -1,9 +1,17 @@
 import sys
 import json
 
-
+if(len(sys.argv) < 2):
+   print "Usage: #SparkEventLogJsonFileName #numCores (Optional, default=80)"
 log = open( sys.argv[1], 'r' )
+numCores = 80
+if (len(sys.argv) >2):
+   numCores = int(sys.argv[2])
 print "TaskID, TaskType, Elapsed, Deserialize, DeserializeCPU, Serialize"
+totalElapsed = 0.0
+totalDeserialize = 0.0
+totalDeserializeCPU = 0.0
+totalSerialize = 0.0
 for line in log:
     blob = json.loads( line )
     if blob['Event'] == 'SparkListenerTaskEnd':
@@ -20,3 +28,21 @@ for line in log:
             deserialize_cpu_ms * 1.0,
             result_serialize_ms * 1.0
             )
+        totalElapsed += elapsed_ms
+        totalDeserialize += deserialize_ms
+        totalDeserializeCPU += deserialize_cpu_ms
+        totalSerialize += result_serialize_ms
+
+print "%.2f, %.2f, %.2f, %.2f" % (
+             totalElapsed / 1000.0,
+             totalDeserialize / 1000.0,
+             totalDeserializeCPU / 1000.0,
+             totalSerialize / 1000.0
+             )
+
+print "%.2f, %.2f, %.2f, %.2f" % (
+             totalElapsed / 1000.0 / 80.0,
+             totalDeserialize / 1000.0 / 80.0,
+             totalDeserializeCPU / 1000.0 / 80.0,
+             totalSerialize / 1000.0 / 80.0
+             )
